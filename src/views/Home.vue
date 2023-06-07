ea69b51e1db31f19de57df0c6c08a61d1f4db5f1
rizkidarmawan21 <rizkidarmawan.0402102@gmail.com>
2023-06-07 17:00:51 +0700

    initial commit

---
+++ src/views/Home.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Hero from "../components/Hero.vue";
+import About from "../components/About.vue";
+
+const name = "Rizki Darmawan";
+const job = "Web Developer";
+const description =
+  "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit dolorum itaque qui unde quisquam consequatur autem. Eveniet quasi nobis aliquid cumque officiis sed rem iure ipsa! Praesentium ratione atque dolorem?";
+const image = "https://source.unsplash.com/1600x1500/?programming";
+
+
+</script>
+
+<template>
+  <Hero :name="name" :job="job" :description="description" :image="image" />
+  <About
+  small-description="Lorem ipsum dolor sit amet consectetur adipisicing elit."
+  description="Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit dolorum itaque qui unde quisquam consequatur autem. Eveniet quasi nobis aliquid cumque officiis sed rem iure ipsa! Praesentium ratione atque dolorem?"
+  github="rizkidarmawan21"
+  linkedin="rizkidarmawan21"
+  instagram="rizkidarmawan_21"
+  />
+</template>
